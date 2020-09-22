344ae75e4f4c4e28ec7004db620dcff6215930f5
Dan Gohman <dev@sunfishcode.online>
2020-09-22 14:26:33 -0700

    Extend the `vectored_at` tests to support partial reads and writes.

--- tests/vectored_at.rs
+++ tests/vectored_at.rs
@@ -60,10 +60,15 @@ fn read_vectored_at() {
     let mut buf0 = vec![0; 8];
     let mut buf1 = vec![0; 8];
     let mut bufs = vec![IoSliceMut::new(&mut buf0), IoSliceMut::new(&mut buf1)];
-    check!(file.read_vectored_at(&mut bufs, 4));
+    let nread = check!(file.read_vectored_at(&mut bufs, 4));
     assert_eq!(check!(file.stream_position()), 26);
-    assert_eq!(&buf0, b"efghijkl");
-    assert_eq!(&buf1, b"mnopqrst");
+    if nread >= 8 {
+        assert_eq!(&buf0, b"efghijkl");
+    }
+    if nread == 16 {
+        assert_eq!(&buf1, b"mnopqrst");
+    }
+    assert!(nread <= 16);
 }
 
 #[test]
@@ -135,10 +140,15 @@ fn read_vectored() {
     let mut buf1 = vec![0; 8];
     let mut bufs = vec![IoSliceMut::new(&mut buf0), IoSliceMut::new(&mut buf1)];
     check!(file.seek(std::io::SeekFrom::Start(4)));
-    check!(file.read_vectored(&mut bufs));
-    assert_eq!(check!(file.stream_position()), 20);
-    assert_eq!(&buf0, b"efghijkl");
-    assert_eq!(&buf1, b"mnopqrst");
+    let nread = check!(file.read_vectored(&mut bufs));
+    assert_eq!(check!(file.stream_position()), (4 + nread) as u64);
+    if nread >= 8 {
+        assert_eq!(&buf0, b"efghijkl");
+    }
+    if nread == 16 {
+        assert_eq!(&buf1, b"mnopqrst");
+    }
+    assert!(nread <= 16);
 }
 
 #[test]
@@ -152,11 +162,17 @@ fn read_at() {
     check!(write!(&mut file, "abcdefghijklmnopqrstuvwxyz"));
     let mut buf0 = vec![0; 8];
     let mut buf1 = vec![0; 8];
-    check!(file.read_at(&mut buf0, 4));
-    check!(file.read_at(&mut buf1, 12));
+    let nread0 = check!(file.read_at(&mut buf0, 4));
+    let nread1 = check!(file.read_at(&mut buf1, 12));
     assert_eq!(check!(file.stream_position()), 26);
-    assert_eq!(&buf0, b"efghijkl");
-    assert_eq!(&buf1, b"mnopqrst");
+    if nread0 == 8 {
+        assert_eq!(&buf0, b"efghijkl");
+        if nread1 == 8 {
+            assert_eq!(&buf1, b"mnopqrst");
+        }
+    }
+    assert!(nread0 <= 8);
+    assert!(nread1 <= 8);
 }
 
 #[test]
@@ -171,11 +187,17 @@ fn read() {
     let mut buf0 = vec![0; 8];
     let mut buf1 = vec![0; 8];
     check!(file.seek(std::io::SeekFrom::Start(4)));
-    check!(file.read(&mut buf0));
-    check!(file.read(&mut buf1));
-    assert_eq!(check!(file.stream_position()), 20);
-    assert_eq!(&buf0, b"efghijkl");
-    assert_eq!(&buf1, b"mnopqrst");
+    let nread0 = check!(file.read(&mut buf0));
+    let nread1 = check!(file.read(&mut buf1));
+    assert_eq!(check!(file.stream_position()), (4 + nread0 + nread1) as u64);
+    if nread0 == 8 {
+        assert_eq!(&buf0, b"efghijkl");
+        if nread1 == 8 {
+            assert_eq!(&buf1, b"mnopqrst");
+        }
+    }
+    assert!(nread0 <= 8);
+    assert!(nread1 <= 8);
 }
 
 #[cfg(any(not(windows), feature = "cap_std_impls"))]
@@ -233,12 +255,18 @@ fn write_vectored_at() {
     let buf0 = b"EFGHIJKL".to_vec();
     let buf1 = b"MNOPQRST".to_vec();
     let mut bufs = vec![IoSlice::new(&buf0), IoSlice::new(&buf1)];
-    check!(file.write_vectored_at(&mut bufs, 4));
+    let nwritten = check!(file.write_vectored_at(&mut bufs, 4));
     assert_eq!(check!(file.stream_position()), 26);
     let mut back = String::new();
     check!(file.seek(std::io::SeekFrom::Start(0)));
     check!(file.read_to_string(&mut back));
-    assert_eq!(back, "abcdEFGHIJKLMNOPQRSTuvwxyz");
+    if nwritten >= 8 {
+        assert_eq!(&back[..12], "abcdEFGHIJKL");
+    }
+    if nwritten == 16 {
+        assert_eq!(back, "abcdEFGHIJKLMNOPQRSTuvwxyz");
+    }
+    assert!(nwritten <= 16);
 }
 
 #[test]
@@ -313,12 +341,15 @@ fn write_vectored() {
     let buf0 = b"EFGHIJKL".to_vec();
     let buf1 = b"MNOPQRST".to_vec();
     let mut bufs = vec![IoSlice::new(&buf0), IoSlice::new(&buf1)];
-    check!(file.write_all_vectored(&mut bufs));
-    assert_eq!(check!(file.stream_position()), 42);
+    let nwritten = check!(file.write_vectored(&mut bufs));
+    assert_eq!(check!(file.stream_position()), (26 + nwritten) as u64);
     let mut back = String::new();
     check!(file.seek(std::io::SeekFrom::Start(0)));
     check!(file.read_to_string(&mut back));
-    assert_eq!(back, "abcdefghijklmnopqrstuvwxyzEFGHIJKLMNOPQRST");
+    assert_eq!(
+        back,
+        &"abcdefghijklmnopqrstuvwxyzEFGHIJKLMNOPQRST"[..26 + nwritten]
+    );
 }
 
 #[test]
@@ -332,13 +363,21 @@ fn write_at() {
     check!(write!(&mut file, "abcdefghijklmnopqrstuvwxyz"));
     let buf0 = b"EFGHIJKL".to_vec();
     let buf1 = b"MNOPQRST".to_vec();
-    check!(file.write_at(&buf0, 4));
-    check!(file.write_at(&buf1, 12));
+    let nwritten0 = check!(file.write_at(&buf0, 4));
+    let nwritten1 = check!(file.write_at(&buf1, 12));
     assert_eq!(check!(file.stream_position()), 26);
     let mut back = String::new();
     check!(file.seek(std::io::SeekFrom::Start(0)));
     check!(file.read_to_string(&mut back));
-    assert_eq!(back, "abcdEFGHIJKLMNOPQRSTuvwxyz");
+    if nwritten0 == 8 {
+        if nwritten1 == 8 {
+            assert_eq!(back, "abcdEFGHIJKLMNOPQRSTuvwxyz");
+        } else {
+            assert_eq!(&back.as_bytes()[0..4 + nwritten0], b"abcdEFGHIJKL");
+        }
+    }
+    assert!(nwritten0 <= 8);
+    assert!(nwritten1 <= 8);
 }
 
 #[test]
@@ -352,11 +391,22 @@ fn write() {
     check!(write!(&mut file, "abcdefghijklmnopqrstuvwxyz"));
     let buf0 = b"EFGHIJKL".to_vec();
     let buf1 = b"MNOPQRST".to_vec();
-    check!(file.write(&buf0));
-    check!(file.write(&buf1));
-    assert_eq!(check!(file.stream_position()), 42);
+    let nwritten0 = check!(file.write(&buf0));
+    let nwritten1 = check!(file.write(&buf1));
+    assert_eq!(
+        check!(file.stream_position()),
+        (26 + nwritten0 + nwritten1) as u64
+    );
     let mut back = String::new();
     check!(file.seek(std::io::SeekFrom::Start(0)));
     check!(file.read_to_string(&mut back));
-    assert_eq!(back, "abcdefghijklmnopqrstuvwxyzEFGHIJKLMNOPQRST");
+    if nwritten0 == 8 {
+        if nwritten1 == 8 {
+            assert_eq!(back, "abcdefghijklmnopqrstuvwxyzEFGHIJKLMNOPQRST");
+        } else {
+            assert_eq!(&back.as_bytes()[22..26 + nwritten0], b"wxyzEFGHIJKL");
+        }
+    }
+    assert!(nwritten0 <= 8);
+    assert!(nwritten1 <= 8);
 }
