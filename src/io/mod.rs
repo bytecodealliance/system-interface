1211af803c597a427db51d9cb8a6af9e8ca7b492
Dan Gohman <dev@sunfishcode.online>
2022-11-27 07:16:22 -0800

    Update to io-extras 0.17, and drop `IsTerminal`.
    The `IsTerminal` trait is now provided by the `is-terminal` crate.

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -2,12 +2,10 @@
 
 mod io_ext;
 mod is_read_write;
-mod is_terminal;
 mod peek;
 mod read_ready;
 
 pub use io_ext::IoExt;
 pub use is_read_write::IsReadWrite;
-pub use is_terminal::IsTerminal;
 pub use peek::{peek_from_bufread, Peek};
 pub use read_ready::ReadReady;
